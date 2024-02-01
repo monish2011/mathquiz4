1d9818938120fd0d5e97b5a2837224219aac4cad
monish2011 <142167411+monish2011@users.noreply.github.com>
2024-02-01 18:51:00 -0500

    Add files via upload

--- quiz_game.js
+++ quiz_game.js
@@ -14,9 +14,43 @@ document.getElementById("number2").innerHTML="";
 function check(){
     get_awnser=document.getElementById("input_check_box").value;
     if(get_awnser==actual_awnser){
-        if(anwser_turn == "player1"){
-            update_player1_score= playerA_score+1;
+        if (anwser == word){
+            if(anwser_turn == "player_1"){
+                playerA_score = playerA_score + 1;
+                document.getElementById("playerA_score").innerHTML = playerA_score;
+    
+            }
+            else
+                if(anwser_turn == "player_B"){
+                    playerB_score = playerB_score + 1;
+                    document.getElementById("playerB_score").innerHTML = playerB_score;
+        
+    
+            }
         }
-
-    }
+            if(question_turn == "player_A"){
+                question_turn = "player_B";
+                document.getElementById("player_question").innerHTMl = "Question turn -" + playerB_name;
+    
+    
+            }
+            else{
+                
+                    question_turn = "player_A";
+                    document.getElementById("player_question").innerHTMl = "Question turn -" + playerA_name;
+        
+        
+                }
+                if(anwser_turn == "playerA"){
+                    anwser_turn = "playerB";
+                    document.getElementById("player_anwser").innerHTML = "Anwser turn - " + playerB_name;
+                }
+                else{
+                    anwser_turn = "playerA";
+                    document.getElementById("player_anwser").innerHTML = "Anwser turn - " + playerA_name;
+                }
+    
+        document.getElementById("output").innerHTML="";
+    
+        }
 }
